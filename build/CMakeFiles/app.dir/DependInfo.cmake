dfc91383a4c7a197c0381c423c188bfbcdf91bd1
Mohammad Hossein Altafi <mohammad.ka.altafi@gmail.com>
2021-10-29 18:17:44 +0330

    Final Version?

--- build/CMakeFiles/app.dir/DependInfo.cmake
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -4,6 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/cpp/tamrin1/src/Functions.cpp" "/mnt/d/cpp/tamrin1/build/CMakeFiles/app.dir/src/Functions.cpp.o"
   "/mnt/d/cpp/tamrin1/src/main.cpp" "/mnt/d/cpp/tamrin1/build/CMakeFiles/app.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
